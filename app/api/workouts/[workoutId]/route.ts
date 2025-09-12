84daec5d9f8148f17c521a848c5ae3e911351c9c
Alexandru Gherasim <alexandru.gherasim.dev18@gmail.com>
2025-09-12 23:07:37 +0300

    Modified the workoutId route DELETE and GET methods

--- app/api/workouts/[workoutId]/route.ts
+++ app/api/workouts/[workoutId]/route.ts
@@ -3,25 +3,26 @@ import { auth } from "@/app/lib/auth";
 import { deleteWorkout } from "@/app/lib/deleteWorkout";
 import { getWorkoutById } from "@/app/lib/getWorkoutById";
 
-export async function DELETE(request: Request, { params }: { params: { workoutId: string } }) {
+export async function DELETE(request: Request, { params }: { params: Promise<{ workoutId: string }> }) {
   const session = await auth();
   const userId = Number(session?.user.id);
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  const workoutId = Number(params.workoutId);
-  const deletedWorkout = await deleteWorkout(workoutId, userId);
-  return NextResponse.json({success: !!deleteWorkout});
+  const { workoutId } = await params;
+  const workoutIdNum = Number(workoutId);
+  const deletedWorkout = await deleteWorkout(workoutIdNum, userId);
+  return NextResponse.json({success: !!deletedWorkout});
 }
 
-export async function GET(request: Request, context: Promise<{params: {workoutId: string}}>) {
+export async function GET(request: Request, { params }: { params: Promise<{ workoutId: string }> }) {
   const session = await auth();
-  const { params } = await context;
   const userId = Number(session?.user.id);
   if (!userId) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  const workoutId = Number(params.workoutId);
-  const workoutToEdit = await getWorkoutById(workoutId, userId);
+  const { workoutId } = await params;
+  const workoutIdNum = Number(workoutId);
+  const workoutToEdit = await getWorkoutById(workoutIdNum, userId);
   return NextResponse.json(workoutToEdit);
 }
